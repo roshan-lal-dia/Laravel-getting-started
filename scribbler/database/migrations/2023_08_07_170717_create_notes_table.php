f3aacd2a5a965014a536f127ae00fb8647e5297d
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-07 22:39:04 +0530

    remove boilerplate in migration create-notes-table

--- scribbler/database/migrations/2023_08_07_170717_create_notes_table.php
+++ scribbler/database/migrations/2023_08_07_170717_create_notes_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            
         });
     }
 
