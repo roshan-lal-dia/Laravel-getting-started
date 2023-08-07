738bfdcc07735c22af7d4c27a99d79120868e309
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-07 22:43:49 +0530

    Update 2023_08_07_170717_create_notes_table.php

--- scribbler/database/migrations/2023_08_07_170717_create_notes_table.php
+++ scribbler/database/migrations/2023_08_07_170717_create_notes_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notes', function (Blueprint $table) {
+            $table->id();
             $table->string('title');
             $table->string('description');
             $table->timestamps();
