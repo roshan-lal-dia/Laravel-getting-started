9eecd78a433cdbd78a56628ca8983b2a8ca6995e
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-08 19:57:41 +0530

    added UI scaffolding

--- scribbler/routes/web.php
+++ scribbler/routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', function () {
 //Route::get('/notes/{note}', [NotesController::class, 'show']);
 
 Route::resource('notes', NotesController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
