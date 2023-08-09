6cf017d6ec2232ba66a6051df7903750fd6bb936
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-09 20:04:26 +0530

    for providing create/edit

--- scribbler/resources/views/notes/create-edit.blade.php
+++ scribbler/resources/views/notes/create-edit.blade.php
@@ -14,14 +14,14 @@
                             <label for="Title" class="col-md-4 col-form-label text-md-end">{{ __('Title') }}</label>
 
                             <div class="col-md-6">
-                                <input id="title" type="text" class="form-control" name="title" value="{{$note->title}}" required >
+                                <input id="title" type="text" class="form-control" name="title" value="{{ $isNote ? $note->title : ''}}" required >
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="Description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
 
                             <div class="col-md-6">
-                                <textarea id="description" type="text" class="form-control" name="description" required >{{$note->description}}</textarea>
+                                <textarea id="description" type="text" class="form-control" name="description" required >{{$isEdit ? $note->description : '' }}</textarea>
                             </div>
                         </div>
                         <button type="submit">{{ $isEdit ? "Update" : "Create"}}</button>
