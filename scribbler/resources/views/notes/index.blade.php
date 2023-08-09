03713c74095c93527e8dc87bc77ee29e90cad2f3
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-09 20:18:24 +0530

    added table strutcure

--- scribbler/resources/views/notes/index.blade.php
+++ scribbler/resources/views/notes/index.blade.php
@@ -4,6 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <table>
+            <thead>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
         </div>
     </div>
 </div>
