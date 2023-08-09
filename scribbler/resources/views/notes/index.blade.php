69e7a9afd9debf3466f7f6103bf024b32b061978
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-09 20:35:29 +0530

    added create notes button

--- scribbler/resources/views/notes/index.blade.php
+++ scribbler/resources/views/notes/index.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        <a href="{{route('notes.create')}}">Create a Note </a>
         <table class="table">
             <thead>
             <tr>
