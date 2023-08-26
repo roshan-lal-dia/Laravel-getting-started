d6e83a590a36603adc26e70ae6f2e1137df1cb81
Roshan Lal Dia <91119120+roshan-lal-dia@users.noreply.github.com>
2023-08-26 20:10:38 +0530

    added old() to get old typed errored values

--- scribbler/resources/views/notes/create-edit.blade.php
+++ scribbler/resources/views/notes/create-edit.blade.php
@@ -24,14 +24,14 @@
                             <label for="Title" class="col-md-4 col-form-label text-md-end">{{ __('Title') }}</label>
 
                             <div class="col-md-6">
-                                <input id="title" type="text" class="form-control" name="title" value="{{ $isEdit ? $note->title : ''}}" required >
+                                <input id="title" type="text" class="form-control" name="title" value="{{ $isEdit ? $note->title : old('title')}}" required >
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="Description" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
 
                             <div class="col-md-6">
-                                <textarea id="description" type="text" class="form-control" name="description" required >{{ $isEdit ? $note->description : '' }}</textarea>
+                                <textarea id="description" type="text" class="form-control" name="description" required >{{ $isEdit ? $note->description : old('description') }}</textarea>
                             </div>
                         </div>
                         <button type="submit">{{ $isEdit ? "Update" : "Create"}}</button>
